1a1098cc9ee8d1c3539c468c8bbebafcb1ce69b4
Joshua Duke <dukejoshuae@gmail.com>
2020-02-11 11:33:03 -0500

    Retrieved scores for the home team

--- app.js
+++ app.js
@@ -2,17 +2,10 @@ let express = require("express");
 let app = express();
 let request = require('request');
 
-request('https://balldontlie.io/api/v1/players/237', (err, res, body)=>{
-  if (!err && res.statusCode == 200){
-    console.log(body);
-  } else {
-    console.log("ERR")
-  }
-})
-
-// request('https://www.google.com', (err, res, body)=>{
+// request('http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/28', (err, res, body)=>{
 //   if (!err && res.statusCode == 200){
-//     console.log(body);
+//     let parsedData = JSON.parse(body)
+//     console.log(parsedData);
 //   } else {
 //     console.log("ERR")
 //   }
@@ -22,8 +15,23 @@ app.set("view engine", "ejs"); // for ejs files
 app.use(express.static(__dirname + "/public")); // for css
 
 app.get("/", (req, res)=>{
-  res.render("index");
+  res.send("This is the homepage");
+  // res.render("");
 }); 
+
+app.get("/scores", (req, res)=>{
+  request('http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard', (err, response, body)=>{
+    if (!err && response.statusCode == 200){
+
+      let score = JSON.parse(body);
+      // res.send(score["events"][0]["competitions"][0]["competitors"][0]["score"]); // score
+      res.render("scores", {score: score});
+
+    } else {
+      console.log("ERR")
+    }
+  })
+})
 
 app.listen(3000, process.env.PORT, ()=>{
   console.log("This server has started");
