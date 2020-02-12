4b886d0877e78f80ab5a4abe9e5c80a4f10fb026
Joshua Duke <dukejoshuae@gmail.com>
2020-02-12 12:02:33 -0500

    added route for teams page, only 25 out of 30 teams available trying to find solution

--- app.js
+++ app.js
@@ -2,36 +2,12 @@ let express = require("express");
 let app = express();
 let request = require('request');
 
-// request('http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/28', (err, res, body)=>{
-//   if (!err && res.statusCode == 200){
-//     let parsedData = JSON.parse(body)
-//     console.log(parsedData);
-//   } else {
-//     console.log("ERR")
-//   }
-// })
-
 app.set("view engine", "ejs"); // for ejs files
 app.use(express.static(__dirname + "/public")); // to read css
 
 app.get("/index", (req, res)=>{
   res.render("index");
 }); 
-
-
-// app.get("/google/logo", function(req, res) {
-//   var requestSettings = {
-//       url: 'https://www.google.com/images/srpr/logo11w.png',
-//       method: 'GET',
-//       encoding: null
-//   };
-
-//   request(requestSettings, function(error, response, body) {
-//       res.set('Content-Type', 'image/png');
-//       res.send(body);
-//   });
-// });
-
 
 app.get("/images", (req, res)=>{
 
@@ -69,6 +45,31 @@ app.get("/scores", (req, res)=>{
       console.log("ERR")
     }
   });
+});
+
+app.get("/teams", (req, res)=>{
+
+  // request('http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams', (err, response, body)=>{
+  //   if (!err && response.statusCode == 200){
+  //     let team = JSON.parse(body)
+
+  //     res.render("teams", {team: team})
+  //   }
+  // });
+
+  let url = "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams"
+
+  request(url, (err, response, body)=>{
+    if (!err && response.statusCode == 200){
+      let team = JSON.parse(body)
+
+      // res.render("teams_test", {team: team})
+      res.send(team);
+    }
+  });
+
+
+
 });
 
 app.listen(3000, process.env.PORT, ()=>{
